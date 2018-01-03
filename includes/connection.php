7c5933b0ecbff5519cba71b90c53bc15e3cdd089
nishant <nishantshetty92@gmail.com>
2018-01-03 20:00:10 +0530

    removed deprecation warnings by adding a line in connections.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,7 @@
 <?php 
+	error_reporting(E_ALL ^ E_DEPRECATED);
 	date_default_timezone_set('Asia/Kolkata');
-	$connect=mysql_connect('localhost','theseus','theseus123')
+	$connect = mysql_connect('localhost','tracker','tracker123')
 	or die('Could Not Connect:'.mysql_error());
 	mysql_select_db('theseus_social') or die('Could not select DB');
 ?>
